80aeefb9d305e296af25d949c51006438efda8f8
Ayushagni01 <ayush.ayush24@gmail.com>
2023-06-10 10:57:06 +0530

    microService-4 | Add Payment Service
    Create: Initial Project and Add require Dependencies

---
+++ PaymentServicet/src/main/java/com/PaymentServicet/PaymentServicet/PaymentServicetApplication.java
@@ -0,0 +1,13 @@
+package com.PaymentServicet.PaymentServicet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServicetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServicetApplication.class, args);
+	}
+
+}
---
+++ PaymentServicet/src/test/java/com/PaymentServicet/PaymentServicet/PaymentServicetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PaymentServicet.PaymentServicet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServicetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
