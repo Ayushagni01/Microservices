e7cbf330c42e355a91e84bd7f21fc82f1682bd3f
Ayushagni01 <ayush.ayush24@gmail.com>
2023-06-12 21:49:24 +0530

    microService-5 | Add API GateWay Service
    Create: Add Api GateWay.

--- PaymentServicet/src/main/java/com/PaymentServicet/PaymentServicet/controller/PaymentController.java
+++ PaymentServicet/src/main/java/com/PaymentServicet/PaymentServicet/controller/PaymentController.java
@@ -12,19 +12,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/payment")
 public class PaymentController {
-
     @Autowired
     private PaymentService  paymentService;
-
-
     @PostMapping
     public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest){
         return new ResponseEntity<>(paymentService.doPayment(paymentRequest), HttpStatus.OK);
     }
-
     @GetMapping("/{orderId}")
     public ResponseEntity<PaymentResponse> getPaymentDetailsByOrderId(@PathVariable("orderId") long orderId){
         return  new ResponseEntity<>(paymentService.getPaymentDetailsByOrderId(orderId),HttpStatus.OK);
     }
-
 }
